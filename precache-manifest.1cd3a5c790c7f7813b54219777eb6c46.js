32a749a9c20960b16042cf202cc245c2d2492011
Bopedig <alex11owen@hotmail.co.uk>
2020-05-15 12:40:46 +0100

    Updates

---
+++ precache-manifest.1cd3a5c790c7f7813b54219777eb6c46.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa307378e83f0eb569e271b93f0f7b95",
+    "url": "/Pokedex/index.html"
+  },
+  {
+    "revision": "6ac6b0f9a3fb37f10869",
+    "url": "/Pokedex/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "0ef00d7be4539032a137",
+    "url": "/Pokedex/static/css/main.f12d6f1d.chunk.css"
+  },
+  {
+    "revision": "6ac6b0f9a3fb37f10869",
+    "url": "/Pokedex/static/js/2.d3920124.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Pokedex/static/js/2.d3920124.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ef00d7be4539032a137",
+    "url": "/Pokedex/static/js/main.95fa0f6e.chunk.js"
+  },
+  {
+    "revision": "371199baa850ce9fe395",
+    "url": "/Pokedex/static/js/runtime-main.52e390ef.js"
+  },
+  {
+    "revision": "8854bb968082eff0c96a04f5b3674c52",
+    "url": "/Pokedex/static/media/spinner.8854bb96.gif"
+  }
+]);
